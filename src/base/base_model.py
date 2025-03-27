b2e176d4b34602ecdaead290afbfebbc5af53e03
Dương Xuân Bách <bachduong7103@gmail.com>
2025-03-27 10:59:31 +0700

    Add new Pipeline

---
+++ src/base/base_model.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel
+
+
+class CustomBaseModel(BaseModel):
+    class Config:
+        """Configuration of the Pydantic Object"""
+
+        # Allowing arbitrary types for class validation
+        arbitrary_types_allowed = True
